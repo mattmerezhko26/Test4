97361eace54d1166cdd78f66383a5344c6387440
mattmerezhko26 <merezhkomatt@gmail.com>
2024-11-07 14:27:43 -0500

    Second commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const HTTP_PORT = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {
   res.render("index");
 });
