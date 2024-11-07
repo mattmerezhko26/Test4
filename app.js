824350be7a140a75b81f05c2d3e8d6d05af926d0
mattmerezhko26 <merezhkomatt@gmail.com>
2024-11-07 14:25:09 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const data = require("./test4.js");
+const HTTP_PORT = process.env.PORT || 4000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.get('/users', (req, res) => {
+  res.render("users", { users: data.users });
+});
+
+
+app.get('/products', (req, res) => {
+  res.render("products", { products: data.products });
+});
+app.get('/filter',(req,res) =>{
+  const filter = data.products.filter(products => products.PID > 3);
+  res.render("Filter",{products:filter});
+});
+app.listen(HTTP_PORT, () => {
+  console.log(`Server listening on: ${HTTP_PORT}`);
+});
---
+++ test4.js
@@ -0,0 +1,40 @@
+const data = {
+  users: [
+    {
+      id:127769230,
+      first_name: "Matvii",
+      last_name:"Merezhko",
+      age:20
+    },
+    {
+      id:123456789,
+      first_name:"Homer",
+      last_name:"Simpson",
+      age:40
+    },
+    {
+      id:987654321,
+      first_name:"Bart",
+      last_name:"Simpsom",
+      age:10
+    }
+  ],
+  products: [
+    {
+      PID:1234,
+      name:"Iphone",
+      price:1000
+    },
+    {
+      PID:5678,
+      name:"Laptop",
+      price:600
+    },
+    {
+      PID:9876,
+      name:"Car",
+      price:10000
+    }
+  ]
+};
+module.exports=data;
